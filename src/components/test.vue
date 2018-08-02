95eaa8032a0854675f8041ad0829dc8c055fb081
sbowen88 <sbowey@gmail.com>
2018-08-02 15:03:42 -0600

    first commit

---
+++ src/components/test.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="test">
+        <h1>{{title}}</h1>
+        <p v-if = "showName">{{user.firstName}}</p>
+        <p v-else></p>
+        <ul>
+            <li v-for = "item in items">{{item.title}}</li>
+        </ul>
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: "test",
+  data() {
+    return {
+      title: "Hello World",
+      user: {
+        firstName: "Scott",
+        lastName: "Smith"
+      },
+      showName: false,
+      items: [{ title: "Item one" }, {title: "Item two"}, {title: "Item three"}]
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
+
